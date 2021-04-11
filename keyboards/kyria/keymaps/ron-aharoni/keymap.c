c1780db2cbf1bb38807f11a17288979e7b1eed7e
Ron Aharoni <aharoni.ron@gmail.com>
2021-04-11 23:05:41 +0300

    keymap

--- keyboards/kyria/keymaps/ron-aharoni/keymap.c
+++ keyboards/kyria/keymaps/ron-aharoni/keymap.c
@@ -40,8 +40,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
       KC_ESC,       KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE,
       KC_TAB,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      LSFT_T(KC_GRV),   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   MO(2),   MO(3), MO(3), MO(2), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_MINS),
-              KC_LCTL, KC_LALT, KC_LGUI, LT(1, KC_SPC), KC_ESC,     KC_ENT, LT(1, KC_SPC), KC_BSPC,  KC_DEL, KC_RCTL
+      LSFT_T(KC_GRV),   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   MO(2),  MO(3), MO(3), MO(2), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_MINS),
+              KC_LCTL, KC_LALT, KC_LGUI, LT(1, KC_SPC), LSFT_T(KC_ESC),     KC_ENT, LT(1, KC_SPC), KC_BSPC,  KC_DEL, KC_RCTL
     ),
 /*
  * Lower Layer: Symbols
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
       _______, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+                                 _______, _______, KC_LGUI, _______, _______, _______, _______, _______, _______, _______
     ),
 /*
  * Adjust Layer: Function keys, RGB
@@ -99,8 +99,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_ADJUST] = LAYOUT(
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6,                                         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,
-      _______, RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI, RGB_MOD,                                     _______, _______, _______, _______, _______,  _______,
-      _______, _______, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD,_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______,  _______,
+      _______, _______, _______, _______, _______, _______,_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 // /*
